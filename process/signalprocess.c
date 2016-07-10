9f9ca4a13d822509ffc7da5e8b7fda06afea417b
yangjiangfeng <yjfdl123@163.com>
2016-07-10 10:10:23 -0400

    waitpid

--- process/signalprocess.c
+++ process/signalprocess.c
@@ -1,16 +1,39 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void handler_int(int  sx){
 	printf("accept %d\n",sx);
 	return;
 }
 
+void handler_childquit(int  sx){
+	printf("accept %d\n",sx);
+	int pid;
+	while( (pid=waitpid(-1,NULL,0))>0){
+		printf("process %d exit\n",pid);
+	}
+	return;
+}
+
 int main(){
 	printf("begin\n");
 	signal(SIGINT,handler_int);
+	signal(SIGCHLD,handler_childquit);
 
 	printf("wait:\n");
+
+	int i;
+	int pid;
+	for (i=0;i<5;i++){
+		pid=fork();
+		if (pid ==0){
+			printf("pid %d sleep %ds\n",getpid(),i);
+			sleep(i);
+			exit(0);
+		}
+		sleep(1);
+	}
 	pause();
 	printf("yes \n");
 }
